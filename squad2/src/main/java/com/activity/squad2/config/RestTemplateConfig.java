c32ad926ff6a02b9fe1f50f4609c3569567fa2a3
Patricia Mae Quitoriano <Patricia.Mae.Quitoriano@ibm.com>
2025-03-11 11:06:28 +0800

    Update Final

--- squad2/src/main/java/com/activity/squad2/config/RestTemplateConfig.java
+++ squad2/src/main/java/com/activity/squad2/config/RestTemplateConfig.java
@@ -1,5 +1,8 @@
 package com.activity.squad2.config;
 
+import com.bpi.framework.web.configurer.client.RestApiConfigurerComponent;
+import com.bpi.framework.web.configproperties.HttpApiConfigProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -7,8 +10,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
+
+    private final RestApiConfigurerComponent restApiConfigurerComponent;
+    private final HttpApiConfigProperties httpApiConfigProperties;
+
+    public RestTemplateConfig(RestTemplateBuilder restTemplateBuilder, HttpApiConfigProperties httpApiConfigProperties, HttpApiConfigProperties httpApiConfigProperties1) {
+        this.httpApiConfigProperties = httpApiConfigProperties;
+        this.restApiConfigurerComponent = new RestApiConfigurerComponent();
+    }
+
     @Bean
     public RestOperations restOperations() {
-        return new RestTemplate();
+        // Use the configure() method to create a RestTemplate
+        return restApiConfigurerComponent.configure(httpApiConfigProperties, RestTemplate.class);
     }
 }
